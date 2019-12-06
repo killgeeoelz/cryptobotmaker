cdb009c1791d81f663e20e5a4de189fedfcf90cf
killgeeoelz <50540215+killgeeoelz@users.noreply.github.com>
2019-12-06 18:43:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-<?php echo "Silence is golden";
+<?php
+if($msg == "/start")
+{
+sm($chatID, "Il Bot funziona!
+Tastiera normale: /tastiera
+Tastiera inline: /itastiera
+Iscritti: /iscritti
+Feedback: /feedback
+Foto: /foto");
+}
