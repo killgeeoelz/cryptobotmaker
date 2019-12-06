b7e1ae37bbb889e2c13ecf2cf675c0cf47590570
killgeeoelz <50540215+killgeeoelz@users.noreply.github.com>
2019-12-06 18:47:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,26 @@
 <?php
+
+$api = $_GET['api'];
+$idadmin = $_GET['admin'];
+$adminID = $idadmin;
+$userbot = $_GET['userbot'];
+
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
+
+foreach($plugins as $plugin => $active)
+{
+if($active) include($plugin);
+}
+
+
+$file = "input.json";
+$f2 = fopen($file, 'w');
+fwrite($f2, $content);
+fclose($f2);
+
+
+
 if($msg == "/start")
 {
 sm($chatID, "Il Bot funziona!
@@ -8,3 +30,5 @@ Iscritti: /iscritti
 Feedback: /feedback
 Foto: /foto");
 }
+
+?>
