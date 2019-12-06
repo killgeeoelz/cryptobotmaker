70167480e4d850d8fefd7bb463d2dab8f5b2666f
killgeeoelz <50540215+killgeeoelz@users.noreply.github.com>
2019-12-06 18:39:37 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://agile-springs-93793.herokuapp.com/execute.php';
+$BOT_TOKEN = '1000572652:AAE_MqJ02_iDH-HLTM9AfkPSWYGdOiEF3Go';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
