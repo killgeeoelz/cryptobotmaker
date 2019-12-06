cf28dc534778a053e48adac96f01612f1a0ef43c
killgeeoelz <50540215+killgeeoelz@users.noreply.github.com>
2019-12-06 18:50:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,3 @@
-<?php
-
-$api = $_GET['api'];
-$idadmin = $_GET['admin'];
-$adminID = $idadmin;
-$userbot = $_GET['userbot'];
-
-$content = file_get_contents("php://input");
-$update = json_decode($content, true);
-
-foreach($plugins as $plugin => $active)
-{
-if($active) include($plugin);
-}
-
-
-$file = "input.json";
-$f2 = fopen($file, 'w');
-fwrite($f2, $content);
-fclose($f2);
-
-
-
 if($msg == "/start")
 {
 sm($chatID, "Il Bot funziona!
@@ -31,4 +8,71 @@ Feedback: /feedback
 Foto: /foto");
 }
 
-?>
+//tastiera normale
+
+if($msg == "/tastiera")
+{
+$menu[] = array("voce 1");
+$menu[] = array("voce 2", "voce 3");
+$menu[] = array("voce 5");
+
+
+$text = "Tastiera normale.
+Nascondi tastiera: /nascondi";
+sm($chatID, $text, $menu, '', false, false, false);
+}
+
+if($msg == "/nascondi")
+{
+
+
+$text = "Tastiera Nascosta.";
+sm($chatID, $text, 'nascondi');
+}
+
+//tastiera inline
+
+if($msg == "/itastiera")
+{
+$menu[] = array(
+array(
+"text" => "bottone1",
+"callback_data" => "/test1"),
+array(
+"text" => "bottone2",
+"callback_data" => "/test2"),
+);
+$menu[] = array(
+array(
+"text" => "bottone3",
+"callback_data" => "/test3"),
+);
+sm($chatID, "Tastiera inline.", $menu, 'Markdown', false, false, true);
+}
+
+
+//funzionamento bottoni tastiera
+
+if($msg == "/test1")
+{
+cb_reply($cbid, "NOTIFICA TIPO 1", false);
+}
+
+if($msg == "/test2")
+{
+cb_reply($cbid, "NOTIFICA TIPO 2", true);
+}
+
+if($msg == "/test3")
+{
+cb_reply($cbid, "NOTIFICA TIPO 1", false, $cbmid, "Messaggio Modificato");
+}
+
+
+//foto
+
+if($msg == "/foto")
+{
+si($chatID, "foto.jpg", false, "questa è la didascalia");
+}
+
